a4326209e70f89b2ca4d75de69631eb77d5e4e88
nandeeshcl <nandeeshclakhi@gmail.com>
2020-03-30 09:04:38 +0530

    Till Day 18

---
+++ src/test/java/com/practise/basics/FirstApp.java
@@ -0,0 +1,38 @@
+package com.practise.basics;
+
+public class FirstApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+
+		Sample.main2();
+
+	}
+
+}
+
+class Sample {
+	public static void main2() {
+
+		System.out.println("Hello world 2");
+
+	}
+}
+
+
+//ERROR: cannot contain more than one public/private/protected class in single java file
+/*public class Test
+{
+	
+}
+
+private class Test
+{
+	
+}
+
+protected class Test
+{
+	
+}*/
---
+++ src/test/java/com/practise/basics/VariableScope.java
@@ -0,0 +1,34 @@
+package com.practise.basics;
+
+public class VariableScope {
+
+	public static void main(String[] args) {
+
+		Test test = new Test();
+		test.variableScope();
+
+	}
+
+}
+
+class Test {
+
+	int a = 10;
+	// int a=20; ERROR same scope no duplicate
+
+	void variableScope() {
+
+		int a = 22;
+		// int a=33; ERROR same scope no duplicate
+
+		double d = 12_34_45.55;
+
+		System.out.println("Value of d is: " + d);
+
+		System.out.println("Values of a in Method level is: " + a); // local variable data is printed
+
+		System.out.println("Values of a in Class level is: " + this.a); // Since this is used, class variable data is
+																		// printed
+
+	}
+}
